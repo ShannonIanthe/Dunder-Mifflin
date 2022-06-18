8465ffb8d203a6c5dfcdb47ef334ef315834b495
Shannon Mettry <shannonmettry12@gmail.com>
2022-06-18 14:03:08 +0200

    serve app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@
 const express = require('express');
 //Set up API
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
+
 api.listen(3000, () => {
     console.log('API up and running!');
 });
 
 //Set up the routes
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello, world!')
-})
+// api.get('/', (req, res) => {
+//     console.log(req);
+//     res.json('Hello, world!')
+// })
+
+//Express "static"
+// api.use((req, res, next) => {
+//     console.log('Hello');
+// });
